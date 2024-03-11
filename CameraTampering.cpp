714286c8a8969f76b164dac77073ce4c4af9b443
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2024-03-11 11:49:24 +0530

    Update CameraTampering.cpp

--- CameraTampering.cpp
+++ CameraTampering.cpp
@@ -11,16 +11,17 @@ int main()
     bool ret = cap.read(frame);
     fgmask = fgbg->apply(frame);
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-
+    
     while (true) 
-	{
+    {
         ret = cap.read(frame);
         if (frame.empty()) 
-		{
+        {
             std::cout << "End of frame" << std::endl;
             break;
-        } else 
-		{
+        } 
+        else 
+        {
             int a = 0;
             std::vector<cv::Rect> bounding_rect;
             fgmask = fgbg->apply(frame);
@@ -30,33 +31,31 @@ int main()
             std::vector<std::vector<cv::Point>> contours;
             cv::findContours(fgmask, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
             for (size_t i = 0; i < contours.size(); i++) 
-			{
+            {
                 bounding_rect.push_back(cv::boundingRect(contours[i]));
             }
             for (size_t i = 0; i < contours.size(); i++) 
-			{
+            {
                 if (bounding_rect[i].width >= 40 || bounding_rect[i].height >= 40) 
-				{
+                {
                     a += bounding_rect[i].width * bounding_rect[i].height;
                 }
                 if (a >= static_cast<int>(frame.total()) / 3) 
-				{
+                {
                     cv::putText(frame, "CAMERA TAMPERING DETECTED", cv::Point(5, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 255), 2);
                 }
                 cv::imshow("frame", frame);
             }
         }
-		//releasing camera and closing all the windows
+        //releasing camera and closing all the windows
         int k = cv::waitKey(30) & 0xff;
         if (k == 27) 
-		{
+        {
             break;
         }
     }
-
+    
     cap.release();
     cv::destroyAllWindows();
     return 0;
 }
-/*********************************************************************************************/
-
